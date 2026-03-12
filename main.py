22d913a5a36f747cb8c0845aac2ab46f0c7a5465
Wzz2333 <124106201+Wzz2333@users.noreply.github.com>
2026-03-12 21:11:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-# 🔴 极简测试代码（保证100%能跑）
 from kivy.app import App
 from kivy.uix.label import Label
 
-class MainApp(App):
+class FinalTestApp(App):
     def build(self):
-        # 显示文字，证明打包成功
-        return Label(text="打包成功！🎉", font_size=40, color=(1,1,1,1))
+        return Label(text="兜底测试：如果这个都失败，就是环境问题！", font_size=25)
 
 if __name__ == "__main__":
-    MainApp().run()
+    FinalTestApp().run()
