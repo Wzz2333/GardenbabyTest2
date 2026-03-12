b125bd45632c60178d42fb69cedd4c9be838e7b5
Wzz2333 <124106201+Wzz2333@users.noreply.github.com>
2026-03-12 21:28:24 +0800

    Refactor app class and update label text

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from kivy.app import App
 from kivy.uix.label import Label
 
-class FinalTestApp(App):
+class MyApp(App):
     def build(self):
-        return Label(text="兜底测试：如果这个都失败，就是环境问题！", font_size=25)
+        return Label(text="打包成功！", font_size=50)
 
 if __name__ == "__main__":
-    FinalTestApp().run()
+    MyApp().run()
