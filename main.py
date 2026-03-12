585693fd728581cf4da4983312333fb545872a7f
Wzz2333 <124106201+Wzz2333@users.noreply.github.com>
2026-03-12 21:01:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+# 🔴 极简测试代码（保证100%能跑）
 from kivy.app import App
 from kivy.uix.label import Label
 
-class GardenBabyApp(App):
+class MainApp(App):
     def build(self):
-        return Label(text="打包成功！🎉", font_size=40)
+        # 显示文字，证明打包成功
+        return Label(text="打包成功！🎉", font_size=40, color=(1,1,1,1))
 
 if __name__ == "__main__":
-    GardenBabyApp().run()
+    MainApp().run()
